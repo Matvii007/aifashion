efbd1d6fa9b0f32f7dfdde3fd070f96961364e29
Matvii007 <matvey.gghit4@gmail.com>
2026-04-23 22:10:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -18,39 +18,17 @@ const lightboxVideo = document.getElementById('lightboxVideo');
 const lightboxClose = document.getElementById('lightboxClose');
 
 const videoSources = [
-  { type: 'vimeo', src: 'https://player.vimeo.com/video/1180955964?autoplay=1&title=0&byline=0&portrait=0&dnt=1' },
-  { type: 'local', src: 'ring.mp4' },
-  { type: 'vimeo', src: 'https://player.vimeo.com/video/1180955914?autoplay=1&title=0&byline=0&portrait=0&dnt=1' },
-  { type: 'vimeo', src: 'https://player.vimeo.com/video/1180955898?autoplay=1&title=0&byline=0&portrait=0&dnt=1' }
+  'https://player.vimeo.com/video/1180955964?autoplay=1&title=0&byline=0&portrait=0&dnt=1',
+  'https://player.vimeo.com/video/1186017677?autoplay=1&title=0&byline=0&portrait=0&dnt=1',
+  'https://player.vimeo.com/video/1180955914?autoplay=1&title=0&byline=0&portrait=0&dnt=1',
+  'https://player.vimeo.com/video/1180955898?autoplay=1&title=0&byline=0&portrait=0&dnt=1'
 ];
 
 document.querySelectorAll('.play-btn').forEach((btn, i) => {
   btn.addEventListener('click', (e) => {
     e.stopPropagation();
-    
-    // Remove existing video/iframe
-    const oldMedia = document.getElementById('lightboxVideo');
-    if(oldMedia) oldMedia.remove();
-    
-    const source = videoSources[i];
-    if (source.type === 'vimeo') {
-      const iframe = document.createElement('iframe');
-      iframe.className = 'lightbox-video';
-      iframe.id = 'lightboxVideo';
-      iframe.frameBorder = '0';
-      iframe.allow = 'autoplay; fullscreen';
-      iframe.allowFullscreen = true;
-      iframe.src = source.src;
-      lightbox.insertBefore(iframe, lightbox.querySelector('.vimeo-logo-blocker'));
-    } else {
-      const video = document.createElement('video');
-      video.className = 'lightbox-video';
-      video.id = 'lightboxVideo';
-      video.controls = true;
-      video.autoplay = true;
-      video.src = source.src;
-      lightbox.insertBefore(video, lightbox.querySelector('.vimeo-logo-blocker'));
-    }
+    lightboxVideo.src = '';
+    setTimeout(() => { lightboxVideo.src = videoSources[i]; }, 50);
     
     lightbox.classList.add('active');
     document.body.style.overflow = 'hidden';
@@ -59,11 +37,7 @@ document.querySelectorAll('.play-btn').forEach((btn, i) => {
 
 function closeLightbox() {
   lightbox.classList.remove('active');
-  const media = document.getElementById('lightboxVideo');
-  if(media) {
-    media.src = '';
-    if(media.tagName === 'VIDEO') media.pause();
-  }
+  lightboxVideo.src = '';
   document.body.style.overflow = '';
 }
 
